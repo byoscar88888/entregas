11ed8d30954beb1d9a478c4a6f50d2490bf25525
byoscar88888 <123187398+byoscar88888@users.noreply.github.com>
2023-10-23 20:18:29 +0200

    Add files via upload

---
+++ Mapa mental freeplane.mm
@@ -0,0 +1,216 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Tipos de lenguajes" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="Freemind_Link_1513112588" CREATED="1153430895318" MODIFIED="1697732737593" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#ffffff" zoom="0.269">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_purple_neon_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1102549087" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2b0053" BACKGROUND_COLOR="#ff4fdb" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#585858" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1102549087" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#8001dd" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ff4fdb" BACKGROUND_COLOR="#2b0053">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffff9e" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#ffec56"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2b0053" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#fd004c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#fd004c">
+<icon BUILTIN="yes"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#fd004c"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#8001dd" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+<edge COLOR="#ff4fdb"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#a120f2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#c100fe" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#dd00fd" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#282828" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ff4fdb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Declarativo" POSITION="bottom_or_right" ID="ID_198396868" CREATED="1697732643557" MODIFIED="1697734620419" VGAP_QUANTITY="3 pt">
+<node TEXT="Orientado a Objetos" ID="ID_410302736" CREATED="1697732646731" MODIFIED="1697734567752">
+<node TEXT="Java" ID="ID_1490189678" CREATED="1697734762725" MODIFIED="1697734768623">
+<node TEXT="Lenguaje de programación ampliamente utilizado para codificar aplicaciones web" ID="ID_1230296979" CREATED="1697735345720" MODIFIED="1697735362073">
+<node TEXT="Aplicaciones Empresariales" ID="ID_1827799030" CREATED="1697735368212" MODIFIED="1697735374062"/>
+<node TEXT="Desarrollo Web" ID="ID_1314801649" CREATED="1697735374146" MODIFIED="1697735380714"/>
+<node TEXT="Juegos" ID="ID_1237594630" CREATED="1697735380756" MODIFIED="1697735383976"/>
+</node>
+</node>
+<node TEXT="C++" ID="ID_93112403" CREATED="1697734772536" MODIFIED="1697734775823">
+<node TEXT="Es apreciado por su capacidad de control sobre el hardware y la memoria, lo que lo hace adecuado para aplicaciones que requieren un rendimiento óptimo y bajo nivel" ID="ID_1395665746" CREATED="1697735448507" MODIFIED="1697735491851">
+<node TEXT="Sistemas Operativos" ID="ID_463203582" CREATED="1697735456327" MODIFIED="1697735466125"/>
+<node TEXT="Videojuegos" ID="ID_672403798" CREATED="1697735457099" MODIFIED="1697735469146"/>
+<node TEXT="Aplicaciones de Red" ID="ID_176963080" CREATED="1697735458786" MODIFIED="1697735474320"/>
+</node>
+</node>
+<node TEXT="Phyton" ID="ID_1550834148" CREATED="1697734777616" MODIFIED="1697734781484">
+<node TEXT="Es útil para escribir código del lado del servidor" ID="ID_780296008" CREATED="1697735529112" MODIFIED="1697735539563">
+<node TEXT="Analisis de datos y ciencia de datos" ID="ID_1952913890" CREATED="1697735539571" MODIFIED="1697735557493"/>
+<node TEXT="Automatizacíón de tareas" ID="ID_1352352144" CREATED="1697735549991" MODIFIED="1697735568331"/>
+<node TEXT="Desarrollo de Juegos" ID="ID_349463204" CREATED="1697735550479" MODIFIED="1697735572826"/>
+</node>
+</node>
+</node>
+<node TEXT="Lenguaje de programación de alto nivel" ID="ID_611401363" CREATED="1697732745086" MODIFIED="1697734584187">
+<node TEXT="Phyton" ID="ID_1146959137" CREATED="1697734787045" MODIFIED="1697734788733"/>
+<node TEXT="Ruby" ID="ID_1881985395" CREATED="1697734791976" MODIFIED="1697734798002">
+<node TEXT="Lenguaje de programación muy popular que se utiliza para muchas cosas, desde el desarrollo de aplicaciones web hasta el análisis de datos" ID="ID_1043382575" CREATED="1697735595015" MODIFIED="1697735600658">
+<node TEXT="Aplicaciones empresariales" ID="ID_1259761547" CREATED="1697735602008" MODIFIED="1697735611765"/>
+<node TEXT="Desarrollo de Videojuegos" ID="ID_1055123974" CREATED="1697735612865" MODIFIED="1697735623026"/>
+<node TEXT="Aplicaciones de escritorio" ID="ID_1017231049" CREATED="1697735616301" MODIFIED="1697735629723"/>
+</node>
+</node>
+<node TEXT="Java Script" ID="ID_384040209" CREATED="1697734800736" MODIFIED="1697734841222">
+<node TEXT="Lenguaje de programación que los desarrolladores utilizan para hacer páginas web interactivas" ID="ID_563545265" CREATED="1697735649025" MODIFIED="1697735663850">
+<node TEXT="Desarrollo Web Front-end" ID="ID_396633946" CREATED="1697735665010" MODIFIED="1697735698866"/>
+<node TEXT="Aplicaciones Moviles" ID="ID_175009955" CREATED="1697735665443" MODIFIED="1697735705769"/>
+<node TEXT="Juegos en linea y Aplicaciones 3d" ID="ID_85347388" CREATED="1697735668206" MODIFIED="1697735728993"/>
+</node>
+</node>
+</node>
+<node TEXT="Lenguajes de dominio especifico" ID="ID_147651712" CREATED="1697734596786" MODIFIED="1697734724308" VSHIFT_QUANTITY="6 pt" VGAP_QUANTITY="3 pt">
+<node TEXT="SQL" ID="ID_1136649445" CREATED="1697734850410" MODIFIED="1697734854035">
+<node TEXT="Lenguaje para administrar y consultar información de bases de datos relacionales." ID="ID_665638526" CREATED="1697735729165" MODIFIED="1697735735029" VGAP_QUANTITY="3 pt">
+<node TEXT="Aplicaciones Móviles" ID="ID_1650573772" CREATED="1697735733264" MODIFIED="1697735748937"/>
+<node TEXT="Aplicaciones de Juegos" ID="ID_473873761" CREATED="1697735736151" MODIFIED="1697735756874"/>
+<node TEXT="Sistema de Gestión de Base de Datos" ID="ID_1867041209" CREATED="1697735736661" MODIFIED="1697735767737"/>
+</node>
+</node>
+<node TEXT="VHDL" ID="ID_437018989" CREATED="1697734862306" MODIFIED="1697734866939">
+<node TEXT="Lenguaje de descripción y modelado diseñado para describir la funcionalidad y la organización de los sistemas hardware digitales, placas de circuito, y componentes" ID="ID_1395370975" CREATED="1697735786028" MODIFIED="1697735790334">
+<node TEXT="Diseño de Circuitos integrados" ID="ID_434728265" CREATED="1697735790350" MODIFIED="1697735802018"/>
+<node TEXT="Diseño de Sistemas Embedidos" ID="ID_1576115543" CREATED="1697735792009" MODIFIED="1697735811265"/>
+<node TEXT="Automatización Industrial" ID="ID_594952604" CREATED="1697735792310" MODIFIED="1697735820055"/>
+</node>
+</node>
+</node>
+<node TEXT="Lenguaje de programación de bajo nivel" POSITION="bottom_or_right" ID="ID_994263904" CREATED="1697734609694" MODIFIED="1697734646194">
+<node TEXT="Assembly" ID="ID_341571646" CREATED="1697734871530" MODIFIED="1697734876267">
+<node ID="ID_1611598718" CREATED="1697735852904" MODIFIED="1697735875998"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: #0000c0;">L</span>enguaje de programación de bajo nivel para los computadores, microprocesadores, microcontroladores, y otros circuitos integrados programables.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Sistemas Operativos" ID="ID_458380945" CREATED="1697735875998" MODIFIED="1697735890233"/>
+<node TEXT="Optimización de Código" ID="ID_1649624485" CREATED="1697735876965" MODIFIED="1697735899600"/>
+<node TEXT="Criptografía y Seguridad" ID="ID_777062576" CREATED="1697735879775" MODIFIED="1697735905724"/>
+</node>
+</node>
+<node TEXT="C" ID="ID_1418410823" CREATED="1697734873077" MODIFIED="1697734878062">
+<node TEXT="Se desarrollan tanto aplicaciones como sistemas operativos a la vez que forma la base de otros lenguajes más actuales" ID="ID_831262658" CREATED="1697735997129" MODIFIED="1697736000605">
+<node TEXT="Sistemas Operativos" ID="ID_458529097" CREATED="1697736001095" MODIFIED="1697736011350"/>
+<node TEXT="Desarrollo de Compiladores y Herramientas de Desarrollo" ID="ID_1344612700" CREATED="1697736001964" MODIFIED="1697736021954"/>
+<node TEXT="Herramientas de Análisis y Simulación" ID="ID_869295982" CREATED="1697736002454" MODIFIED="1697736041271"/>
+</node>
+</node>
+</node>
+<node TEXT="Lenguaje de programación de script" ID="ID_231372907" CREATED="1697734652417" MODIFIED="1697734678978">
+<node TEXT="Bash" ID="ID_1418885912" CREATED="1697734882830" MODIFIED="1697734888143">
+<node TEXT="Procesador de comandos que generalmente se ejecuta en una ventana de texto donde el usuario escribe comandos que causan acciones" ID="ID_1756729621" CREATED="1697736068828" MODIFIED="1697736084413">
+<node TEXT="Automatización de Tareas de Sistema" ID="ID_1839162045" CREATED="1697736084423" MODIFIED="1697736095020"/>
+<node TEXT="Procesamiento de Archivos y Datos" ID="ID_1320294746" CREATED="1697736085034" MODIFIED="1697736101215"/>
+<node TEXT="Seguridad y Auditoría de Sistemas" ID="ID_1267114668" CREATED="1697736085431" MODIFIED="1697736107268"/>
+</node>
+</node>
+<node TEXT="Powershell" ID="ID_604587949" CREATED="1697734884339" MODIFIED="1697734890996">
+<node TEXT="Se usa normalmente para automatizar la administración de sistemas" ID="ID_1453539891" CREATED="1697736125004" MODIFIED="1697736198343">
+<node TEXT="Gestión de Servicios Web" ID="ID_711052017" CREATED="1697736127502" MODIFIED="1697736140167"/>
+<node TEXT="Administración de Bases de Datos" ID="ID_656602472" CREATED="1697736130742" MODIFIED="1697736146820"/>
+<node TEXT="Desarrollo de Scripts para Administración" ID="ID_811303770" CREATED="1697736131120" MODIFIED="1697736156714"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Imparativo" POSITION="top_or_left" ID="ID_1925935677" CREATED="1697732751503" MODIFIED="1697734549904">
+<node TEXT="Lenguajes de consulta de bases de datos" ID="ID_950003595" CREATED="1697734995687" MODIFIED="1697735009047">
+<node TEXT="SQL" ID="ID_1853775605" CREATED="1697735037028" MODIFIED="1697735041185"/>
+</node>
+<node TEXT="Lenguajes de hojas de estilo" ID="ID_1765037314" CREATED="1697734996751" MODIFIED="1697735018320">
+<node TEXT="CSS" ID="ID_681463363" CREATED="1697735042541" MODIFIED="1697735049550">
+<node TEXT="Es un lenguaje para la composición y estructuración de páginas web" POSITION="top_or_left" ID="ID_733146930" CREATED="1697736198669" MODIFIED="1697736202410">
+<node TEXT="Diseño de Sitios web" ID="ID_456343640" CREATED="1697736202859" MODIFIED="1697736217546"/>
+<node TEXT="Diseño de Repositorios" ID="ID_1619884684" CREATED="1697736203455" MODIFIED="1697736227771"/>
+<node TEXT="Animaciones y Transiciones" ID="ID_1886261713" CREATED="1697736203897" MODIFIED="1697736234704"/>
+</node>
+</node>
+</node>
+<node TEXT="Lenguajes funcionales" ID="ID_698778179" CREATED="1697734997891" MODIFIED="1697735022594">
+<node TEXT="Haskell" ID="ID_808822181" CREATED="1697735050635" MODIFIED="1697735054924">
+<node TEXT="Enfocado en la programación funcional, así como en la evaluación de expresiones, la evaluación perezosa, la manipulación de funciones y la inmutabilidad" ID="ID_117591292" CREATED="1697736256980" MODIFIED="1697736264038">
+<node TEXT="Desarrollo Web" ID="ID_1165332779" CREATED="1697736264057" MODIFIED="1697736270855"/>
+<node TEXT="Educación y Capacitación" ID="ID_1897463177" CREATED="1697736264608" MODIFIED="1697736278664"/>
+<node TEXT="Herramientas de Desarrollo" ID="ID_1943434067" CREATED="1697736265016" MODIFIED="1697736284700"/>
+</node>
+</node>
+<node TEXT="Lisp" ID="ID_898336590" CREATED="1697735057923" MODIFIED="1697735060605">
+<node TEXT="Lenguaje de programación que fue diseñado para una fácil manipulación de cadenas de datos" ID="ID_848197067" CREATED="1697736303992" MODIFIED="1697736307217">
+<node TEXT="Recursión" ID="ID_1024022279" CREATED="1697736307681" MODIFIED="1697736314280"/>
+<node TEXT="Manipulación de Datos Simbólicos" ID="ID_1673744084" CREATED="1697736308211" MODIFIED="1697736321571"/>
+<node TEXT="Notación de Listas" ID="ID_1167772597" CREATED="1697736308614" MODIFIED="1697736325735"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
